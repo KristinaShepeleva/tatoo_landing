86cb7acc8363dcf441ed18e811a0ffadcbba165a
KristinaShepeleva <shepelevakristina52@gmail.com>
2023-04-03 11:06:57 +0300

    44

---
+++ src/index.js
@@ -0,0 +1,3 @@
+
+import './js/menu_mob';
+import './js/modal';
---
+++ src/js/modal.js
@@ -0,0 +1,80 @@
+// // (() => {
+// //   const refs = {
+// //     openModalBtn: document.querySelector(".data-modal-open"),
+// //     closeModalBtn: document.querySelector(".data-modal-close"),
+// //     modal: document.querySelector(".data-modal"),
+// //   };
+
+// //   refs.openModalBtn.addEventListener("click", toggleModal);
+// //   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+// //   function toggleModal() {
+// //     refs.modal.classList.toggle("is-hidden");
+// //   }
+// // })();
+
+const ESCAPE_BTN_CODE = 'Escape';
+
+const buttonRef = document.querySelector('[data-modal-open]');
+const closeButtonRef = document.querySelector('[data-modal-close]');
+const modalRef = document.querySelector('[data-modal]');
+
+console.log(modalRef);
+
+buttonRef.addEventListener("click", openModal);
+closeButtonRef.addEventListener("click", closeModal);
+
+modalRef.addEventListener('click', (e) => {
+  if (e.currentTarget === e.target) {
+     closeModal();
+   }
+ });
+
+
+function openModal() {
+  console.log(modalRef)
+   modalRef.classList.remove("is-hidden");
+   document.addEventListener('keydown', closeModalByEscape);
+}
+ 
+function closeModal() {
+  modalRef.classList.add("is-hidden");
+  document.removeEventListener('keydown', closeModalByEscape);
+}
+
+function closeModalByEscape(e) {
+  if (e.code === ESCAPE_BTN_CODE) {
+    closeModal();
+  }
+}
+
+
+
+
+
+
+
+// buttonRef.addEventListener('click', openModal);
+// // closeButtonRef.addEventListener('click', closeModal);
+// // modalWrapperRef.addEventListener('click', (e) => {
+// //   if (e.currentTarget === e.target) {
+// //     closeModal();
+// //   }
+// // });
+
+// function openModal() {
+//   console.log(modalRef)
+//    modalRef.classList.add("is-hidden");
+//    //document.addEventListener('keydown', closeModalByEscape);
+// }
+ 
+// // function closeModal() {
+// //   modalWrapperRef.classList.remove('open');
+// //   //document.removeEventListener('keydown', closeModalByEscape);
+// // }
+
+// // function closeModalByEscape(e) {
+// //   if (e.code === ESCAPE_BTN_CODE) {
+// //     closeModal();
+// //   }
+// // }
