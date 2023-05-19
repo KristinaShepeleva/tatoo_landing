8c022fe129a1974518dcf1fe519317c9c76861e8
KristinaShepeleva <shepelevakristina52@gmail.com>
2023-05-19 14:30:24 +0300

    gallery

--- src/js/gallery-item.js
+++ src/js/gallery-item.js
@@ -4,56 +4,168 @@ export const galleryItems = [
       'https://cdn.pixabay.com/photo/2022/11/08/22/15/woman-7579421__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2022/11/08/22/15/woman-7579421_960_720.jpg',
-    description: 'Tattoo',
+    description: 'girl',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/12/08/20/52/tattoo-1893141__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/12/08/20/52/tattoo-1893141_960_720.jpg',
+    description: 'man',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2022/05/17/11/54/woman-7202448__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2022/05/17/11/54/woman-7202448_960_720.jpg',
-    description: 'Tattoo',
+    description: 'girl',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2014/05/21/15/39/tattoo-349891__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2014/05/21/15/39/tattoo-349891_960_720.jpg',
-    description: 'Tattoo',
+    description: 'girl',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2015/04/12/12/36/warrior-718747__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2015/04/12/12/36/warrior-718747_960_720.jpg',
-    description: 'Tattoo',
+    description: 'man',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2016/02/19/11/08/woman-1209592__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2016/02/19/11/08/woman-1209592_960_720.jpg',
-    description: 'Tattoo',
+    description: 'girl',
   },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/12/08/20/52/tattoo-1893141__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/12/08/20/52/tattoo-1893141_960_720.jpg',
-    description: 'Tattoo',
-  },
+ 
   {
     preview:
       'https://cdn.pixabay.com/photo/2017/08/09/16/17/girl-2615028__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2017/08/09/16/17/girl-2615028_960_720.jpg',
-    description: 'Tattoo',
+    description: 'girl',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2016/10/24/15/03/fashion-1766344__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2016/10/24/15/03/fashion-1766344_960_720.jpg',
-    description: 'Tattoo',
+    description: 'man',
   },
-  
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/09/24/10/17/man-6652196__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/09/24/10/17/man-6652196_960_720.jpg',
+    description: 'man',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/07/16/22/09/tattoos-5412442__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/07/16/22/09/tattoos-5412442_960_720.jpg',
+    description: 'man',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/04/12/12/36/warrior-718747__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/04/12/12/36/warrior-718747_960_720.jpg',
+    description: 'man',
+    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2014/05/21/15/39/tattoo-349891__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2014/05/21/15/39/tattoo-349891_960_720.jpg',
+    description: 'girl',
+  },
+    {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/02/19/11/08/tattoos-1209589__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/02/19/11/08/tattoos-1209589_960_720.jpg',
+    description: 'girl',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/09/11/17/39/woman-2739786__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/09/11/17/39/woman-2739786_960_720.jpg',
+    description: 'girl',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/11/22/22/28/tattoo-1057451__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/11/22/22/28/tattoo-1057451_960_720.jpg',
+    description: 'man',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/08/01/12/41/people-2565092__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/08/01/12/41/people-2565092_960_720.jpg',
+    description: 'girl',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/06/01/21/37/protest-4245092__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/06/01/21/37/protest-4245092__340.jpg',
+    description: 'girl',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/12/08/20/52/tattoo-1893141__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/12/08/20/52/tattoo-1893141_960_720.jpg',
+    description: 'man',
+  },
+    {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/11/08/22/15/woman-7579421__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/11/08/22/15/woman-7579421_960_720.jpg',
+    description: 'girl',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/05/17/11/54/woman-7202448__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/05/17/11/54/woman-7202448_960_720.jpg',
+    description: 'girl',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/29/05/18/adult-1867489__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/29/05/18/adult-1867489_960_720.jpg',
+    description: 'man',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/12/13/14/37/hands-3016910__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/12/13/14/37/hands-3016910_960_720.jpg',
+    description: 'man',
+  },
+   {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/08/09/16/17/girl-2615028__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/08/09/16/17/girl-2615028_960_720.jpg',
+    description: 'girl',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/05/27/12/52/tattoo-2348405__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/05/27/12/52/tattoo-2348405_960_720.jpg',
+    description: 'girl',
+  },
 ];
--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,14 +1,17 @@
 import { galleryItems } from './gallery-item';
-import { galleryWoman } from './gallery-woman';
-import { galleryMan } from './gallery-man';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+
 const galleryRef = document.querySelector('.gallary__fotos');
+const prev = document.querySelector('.prev-arrow');
+const next = document.querySelector('.next-arrow');
+const allTatoo = document.querySelector('.all-tatoo');
 const galleryWomanRef = document.querySelector('.gallary__menu-button--woman')
 const galleryManRef = document.querySelector('.gallary__menu-button--man')
 
-const markup = createGallaryMarkup(galleryItems);
+
+let markup = createGallaryMarkup(galleryItems);
 galleryRef.innerHTML = markup;
 
 let simpleLightbox = new SimpleLightbox('.gallary__fotos a', {
@@ -27,26 +30,87 @@ function createGallaryMarkup(image) {
     }).join('');
 };
 
+let thisPage = 1;
+let limit = 8;
+
+let list = document.querySelectorAll('.gallary__fotos .gallary__img');
+
 galleryRef.addEventListener('click', onImageClick);
 
 function onImageClick(event) {
     event.preventDefault();
   }
 
+
 galleryWomanRef.addEventListener('click', onWomanClick)
 
 function onWomanClick(ev) {
     ev.preventDefault();
-    const markupWoman = createGallaryMarkup(galleryWoman);
-    galleryRef.innerHTML = markupWoman;
+      markup = createGallaryMarkup(galleryItems.filter(item => item.description.includes('girl')));
+    galleryRef.innerHTML = markup;
+    list = document.querySelectorAll('.gallary__fotos .gallary__img');
+    loadItem();
     simpleLightbox.refresh()
 }
+
 
 galleryManRef.addEventListener('click', onManClick)
 
 function onManClick(ev) {
     ev.preventDefault();
-    const markupMan = createGallaryMarkup(galleryMan);
-    galleryRef.innerHTML = markupMan;
+    markup = createGallaryMarkup(galleryItems.filter(item => item.description.includes('man')));
+    galleryRef.innerHTML = markup;
+    list = document.querySelectorAll('.gallary__fotos .gallary__img');
+    loadItem();
+    simpleLightbox.refresh();
+}
+
+allTatoo.addEventListener('click', onAllClick)
+
+function onAllClick(ev) {
+    ev.preventDefault();
+    markup = createGallaryMarkup(galleryItems);
+    galleryRef.innerHTML = markup;
+    list = document.querySelectorAll('.gallary__fotos .gallary__img');
+    loadItem();
     simpleLightbox.refresh()
 }
+
+const count = Math.ceil(list.length / limit);
+   
+function loadItem(){
+    let beginGet = limit * (thisPage - 1);
+    let endGet = limit * thisPage - 1;
+    list.forEach((item, key)=>{
+        if(key >= beginGet && key <= endGet){
+            item.style.display = 'block';
+        }
+            else {
+            item.style.display = 'none';
+        }
+    })
+}
+
+loadItem();
+
+prev.setAttribute("disabled", true);
+
+prev.addEventListener("click", () => {
+    thisPage -= 1;
+    next.removeAttribute("disabled");
+    loadItem(); 
+   
+    if (thisPage === 1) {
+        prev.setAttribute("disabled", true);
+    }            
+});   
+
+next.addEventListener("click", () => {
+    thisPage += 1;
+    prev.removeAttribute("disabled");
+    loadItem();
+     
+    if (thisPage === count) {
+    next.setAttribute("disabled", true);
+    }
+});
